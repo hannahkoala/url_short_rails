1b97ce7965f9f8359481729f1dc1052d2f39afa2
Hannah <hunter.hannah@icloud.com>
2015-04-11 14:19:32 -0500

    added root - added clickable links to index

--- db/migrate/20150411190429_track_clicks.rb
+++ db/migrate/20150411190429_track_clicks.rb
@@ -1,10 +1,5 @@
 class TrackClicks < ActiveRecord::Migration
   def change
-  end
-  def up
-    add_column :addresses, :clicks, :integer, :default => 0
-  end
- 
-  def down
+      add_column :addresses, :clicks, :integer, :default => 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(version: 20150411190429) do
   create_table "addresses", force: :cascade do |t|
     t.string   "location"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "clicks",     default: 0
   end
 
 end
